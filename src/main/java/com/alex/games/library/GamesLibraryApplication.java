f677e04bad370abb2348396c06f445eeb1c1abd5
alexNotebook <alex97av@gmail.com>
2024-05-26 12:15:30 +0200

    first commit

---
+++ src/main/java/com/alex/games/library/GamesLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.alex.games.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/games/library/GamesLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.games.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
